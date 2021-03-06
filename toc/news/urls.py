84e5e0a509ed609aaaf8808cf20157498f832469
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2021-03-06 09:33:20 +0300

    Change url patterns for news

--- toc/news/urls.py
+++ toc/news/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('news/', views.NewsListView.as_view(), name='news'),
-    path('news/<int:pk>/', views.NewsDetailView.as_view(), name='news-detail')
+    path('', views.NewsListView.as_view(), name='news'),
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail')
 ]
