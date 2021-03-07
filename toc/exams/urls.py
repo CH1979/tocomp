600c969a2367f30a7279eb8721f9463d447b963d
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2021-03-07 19:32:16 +0300

    Add views and templates for exam
    Change view name
    
    Add views and templates for exam model
    
    Add views and templates for exam model

--- toc/exams/urls.py
+++ toc/exams/urls.py
@@ -3,5 +3,17 @@ from . import views
 
 
 urlpatterns = [
-    path('create/', views.ExamCreate.as_view(), name='exam-create')
+    path('', views.ExamListView.as_view(), name='exam-list'),
+    path('create/', views.ExamCreateView.as_view(), name='exam-create'),
+    path('<int:pk>/', views.ExamDetailView.as_view(), name='exam-detail'),
+    path(
+        '<int:pk>/update/',
+        views.ExamUpdateView.as_view(),
+        name='exam-update'
+    ),
+    path(
+        '<int:pk>/delete/',
+        views.ExamDeleteView.as_view(),
+        name='exam-delete'
+    )
 ]
--- toc/exams/views.py
+++ toc/exams/views.py
@@ -1,8 +1,32 @@
+from django.db.models import Count
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Exam
 
 
-class ExamCreate(CreateView):
+class ExamCreateView(CreateView):
     model = Exam
-    success_url = reverse_lazy('')
+    fields = '__all__'
+    success_url = reverse_lazy('exam-list')
+
+
+class ExamUpdateView(UpdateView):
+    model = Exam
+    fields = '__all__'
+    success_url = reverse_lazy('exam-list')
+
+
+class ExamDeleteView(DeleteView):
+    model = Exam
+    success_url = reverse_lazy('exam-list')
+
+
+class ExamListView(ListView):
+    model = Exam
+    context_object_name = 'exam_list'
+    queryset = Exam.objects.annotate(num_cards=Count('examcard'))
+
+
+class ExamDetailView(DetailView):
+    model = Exam
