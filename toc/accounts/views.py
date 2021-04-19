ac12225f5ffb88efd6760a09d1ead7bf3aa0be3a
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2021-04-19 14:08:04 +0300

    Draft for account views and templates

--- toc/accounts/views.py
+++ toc/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Course, GroupDetail
 
-# Create your views here.
+
+class ProfileView(DetailView):
+    model = User
+
+    def get_object(self):
+        return self.model.objects.filter(id=self.request.user.id)
+
+
+class CourseCreateView(CreateView):
+    model = Course
+    fields = '__all__'
+    success_url = 'group-list'
+
+
+class CourseUpdateView(UpdateView):
+    model = Course
+    fields = '__all__'
+    success_url = 'group-list'
+
+
+class CourseDeleteView(DeleteView):
+    model = Course
+    success_url = 'group-list'
+
+
+class GroupListView(ListView):
+    model = GroupDetail
+    context_object_name = 'group_list'
+    queryset = GroupDetail.objects.filter(year=2020)
+
+
+class GroupCreateView(CreateView):
+    model = GroupDetail
+    fields = '__all__'
