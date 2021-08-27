1ca74b0ead3ad109e722edf424c55b05e7664b32
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2021-08-27 16:20:43 +0300

    Decorate login form as modal

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,10 +1,22 @@
 <template>
 <div>
-  <form @submit="userSignIn">
-    <input type="text" v-model="username">
-    <input type="password" v-model="password">
-    <button>Войти</button>
-  </form>
+  <div class="modal modal-sm active" id="modal-id">
+    <div class="modal-container">
+      <div class="modal-header">
+        <router-link to="/" class="btn btn-clear float-right" aria-label="Close"></router-link>
+        <div class="modal-title h4">Вход</div>
+      </div>
+      <div class="modal-body">
+        <form @submit="userSignIn">
+          <label class="form-label">Имя пользователя</label>
+          <input class="form-input" type="text" v-model="username" placeholder="Имя пользователя">
+          <label class="form-label">Пароль</label>
+          <input class="form-input" type="password" v-model="password" placeholder="Пароль">
+          <button class="btn btn-primary">Войти</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -28,7 +40,7 @@ export default {
       this.$store.dispatch('userLogin', {
         username: this.username,
         password: this.password
-      })
+      }).then(() => this.$router.push({name: 'account'}))
     }
   }
 }
