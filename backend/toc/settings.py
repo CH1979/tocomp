60e351aa327508b5d12b8e63946d8a4b06da3eb0
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-03-15 20:52:14 +0300

    Modify settings for future prod

--- backend/toc/settings.py
+++ backend/toc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3a)vu(xbjb0o57@33sr_6*wao!c#cj117p=y18cegln@_8o$a1'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    '3a)vu(xbjb0o57@33sr_6*wao!c#cj117p=y18cegln@_8o$a1'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
